07a1d0e896380dcf3e11ffaa8c34b57630f8fbfe
QianyueW <qianyueww@gamil.com>
2024-08-06 21:27:05 +0800

    add imgui events

--- Lulu/src/Lulu/Application.cpp
+++ Lulu/src/Lulu/Application.cpp
@@ -81,7 +81,7 @@ void Application::Run()
 
 	while (m_Running)
 	{
-		glClearColor(0, 1, 0, 1);
+		glClearColor(0, 0.5, 0.1, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 		for (Layer* layer : m_LayerStack)
 		{
--- Lulu/src/Lulu/ImGui/ImGuiLayer.h
+++ Lulu/src/Lulu/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Lulu/Layer.h"
 
+#include "Lulu/Events/ApplicationEvent.h"
+#include "Lulu/Events/KeyEvent.h"
+#include "Lulu/Events/MouseEvent.h"
+
 namespace Lulu {
 
 	class LULU_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Lulu {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
